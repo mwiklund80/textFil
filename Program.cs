3ce61ce9be1da5a23ed1e156f4f6b14b4f1d6f7b
Martin Wiklund <martin.wiklund@learnet.se>
2020-01-09 14:16:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace textFil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Exempel med StreamWriter *****");
+            try
+            {
+                // Skapar en StreamWriter-objekt och skriver strängar.
+                using (StreamWriter writer = new StreamWriter("citat.txt"))
+                {
+                    writer.WriteLine("Fantasi är viktigare än kunskap.");
+                    writer.WriteLine("Tärningen är kastad.");
+                    writer.WriteLine("Fånga dagen.");
+                    // Lägger till två nya rader.
+                    writer.Write(writer.NewLine);
+                    writer.WriteLine();
+                }
+                Console.WriteLine("Skapade en fil med några citat...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ett oväntat fel inträffade.\n{0}",
+                ex.Message);
+            }
+
+        }
+    }
+}
